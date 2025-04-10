a7c4fdac145c5f5291fbbf6f30f281834c7f3d05
Draqy <peterh9792@gmail.com>
2025-04-10 20:03:09 +0200

    range voor wat wit is toegevoegd

--- cameratest/lines/linefollowing_visualisation.py
+++ cameratest/lines/linefollowing_visualisation.py
@@ -18,7 +18,7 @@ def get_x(lines):
     return None
 
 def detect_lanes(path):
-    capture = cv2.VideoCapture(path) # kart camera
+    capture = cv2.VideoCapture(path)
 
     if not capture.isOpened():
         print("Error: Could not open video.")
@@ -30,18 +30,21 @@ def detect_lanes(path):
         if not ret:
             break
 
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        blurred = cv2.GaussianBlur(frame, (5, 5), 0)
+        gray = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
+        grayscale = cv2.inRange(gray, 200, 255) #200 - 255
 
-        blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-
-        edges = cv2.Canny(blurred, 50, 150)
+        edges = cv2.Canny(grayscale, 50, 150)
 
         height, width = edges.shape
-        # region_vertices = [(0, height), (width // 2, height // 2), (width, height)]
-        region_vertices = [(0, height // 2), (width, height // 2), (width, height), (0, height)]
-        roi = region_of_interest(edges, np.array([region_vertices], np.int32))
+        
+        #region_vertices = [(0, height // 2), (width, height // 2), (width, height), (0, height)] #onderkant
+        # region_vertices = [(0, 0), (width, 0), (width, height // 2), (0, height // 2)] #bovenkant
+        #roi = region_of_interest(edges, np.array([region_vertices], np.int32))
+        #lines = cv2.HoughLinesP(roi, 1, np.pi / 180, 50, np.array([]), minLineLength=50, maxLineGap=300)
+        
+        lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 50, np.array([]), minLineLength=50, maxLineGap=300)
 
-        lines = cv2.HoughLinesP(roi, 1, np.pi / 180, 50, np.array([]), minLineLength=50, maxLineGap=300)
 
         x_at_target_values = []
 
@@ -59,7 +62,8 @@ def detect_lanes(path):
             elif x_at_target > x_target + 50: #voorbeeld waardes
                 print("steer right")
 
-        cv2.imshow("Lane Detection", frame)
+        # cv2.imshow("Grayscale", grayscale)
+        cv2.imshow("Lines", frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
@@ -67,6 +71,5 @@ def detect_lanes(path):
     capture.release()
     cv2.destroyAllWindows()
 
-path = 'C:\\Vakken\\Project 78 (SDC)\\SDCCLOSED\\cameratest\\lines\\input.mp4'
-# path = 1
+path = 'C:/Vakken/Jaar 2/Project 78 (SDC)/data/smalldemoright/output_video.mp4'
 detect_lanes(path)
