7c48950945ec93dfbbbc1f36b1cba00d9b30b4ec
Draqy <peterh9792@gmail.com>
2025-04-16 21:56:53 +0200

    onnodige files verwijderd

--- cameratest/lines/linefollowing_visualisation.py
+++ cameratest/lines/linefollowing_visualisation.py
@@ -1,8 +1,8 @@
 import cv2
 import numpy as np
 
-y_target = 500
-x_target = 0 #standaard positie van de lijn bij y=500
+y_norm = 500
+x_norm = 575 #standaard positie van de lijn bij y=500
 
 def region_of_interest(img, vertices):
     mask = np.zeros_like(img)
@@ -13,7 +13,7 @@ def region_of_interest(img, vertices):
 def get_x(lines):
     for x1, y1, x2, y2 in lines:
         m = (x2 - x1) / (y2 - y1)
-        x_coord = x1 + (y_target - y1) * m
+        x_coord = x1 + (y_norm - y1) * m
         return int(x_coord)
     return None
 
@@ -34,17 +34,16 @@ def detect_lanes(path):
         gray = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
         grayscale = cv2.inRange(gray, 200, 255) #200 - 255
 
-        edges = cv2.Canny(grayscale, 50, 150)
+        edges = cv2.Canny(grayscale, 70, 200)
 
         height, width = edges.shape
-        
+
         #region_vertices = [(0, height // 2), (width, height // 2), (width, height), (0, height)] #onderkant
         # region_vertices = [(0, 0), (width, 0), (width, height // 2), (0, height // 2)] #bovenkant
         #roi = region_of_interest(edges, np.array([region_vertices], np.int32))
         #lines = cv2.HoughLinesP(roi, 1, np.pi / 180, 50, np.array([]), minLineLength=50, maxLineGap=300)
-        
-        lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 50, np.array([]), minLineLength=50, maxLineGap=300)
 
+        lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 50, np.array([]), minLineLength=50, maxLineGap=300)
 
         x_at_target_values = []
 
@@ -57,9 +56,9 @@ def detect_lanes(path):
 
         if x_at_target_values:
             x_at_target = int(np.mean(x_at_target_values))
-            if x_at_target < x_target - 50: #voorbeeld waardes
+            if x_at_target < x_norm - 50: #voorbeeld waardes
                 print("steer left")
-            elif x_at_target > x_target + 50: #voorbeeld waardes
+            elif x_at_target > x_norm + 50: #voorbeeld waardes
                 print("steer right")
 
         # cv2.imshow("Grayscale", grayscale)
@@ -71,5 +70,5 @@ def detect_lanes(path):
     capture.release()
     cv2.destroyAllWindows()
 
-path = 'C:/Vakken/Jaar 2/Project 78 (SDC)/data/smalldemoright/output_video.mp4'
+path = 'C:/Vakken/Jaar 2/Project 78 (SDC)/data/50pdemo/output_video.mp4'
 detect_lanes(path)
