c2a344d2826aca07295eefe6005eb044309025f9
Draqy <peterh9792@gmail.com>
2025-04-16 23:36:37 +0200

    Update test2.py

--- cameratest/lines/test2.py
+++ cameratest/lines/test2.py
@@ -42,9 +42,11 @@ def detect_lanes(path):
 
         height, width = edges.shape
         y_norm = int(height / 2)
-        x_norm = 600
+        x_norm = 575
 
-        region_vertices = [(width // 2, 0), (width, 0), (width, height), (width // 2, height)] # rechterkant
+        roi_border = width // 5 * 2
+
+        region_vertices = [(roi_border, 0), (width, 0), (width, height), (roi_border, height)] # rechterkant
         roi = region_of_interest(edges, np.array([region_vertices], np.int32))
         lines = cv2.HoughLinesP(roi, 1, np.pi / 180, 50, np.array([]), minLineLength=50, maxLineGap=300)
 
@@ -63,25 +65,25 @@ def detect_lanes(path):
         if x_at_target_values:
             x_at_target = int(np.median(x_at_target_values))
             cv2.circle(frame, (x_at_target, y_norm), 10, (255, 0, 0), -1)  # visualisation of x_at_target
-            if x_at_target < x_norm - 50: # example values
+            if x_at_target < x_norm - 25: # example values
                 print("steer left")
-            elif x_at_target > x_norm + 50: # example values
+            elif x_at_target > x_norm + 25: # example values, maybe make them linear / exponential
                 print("steer right")
             else:
-                print("go straight")    
+                print("go straight")
 
         cv2.line(frame, (0, y_norm), (width, y_norm), (255, 255, 0), 2) # y_norm
         cv2.line(frame, (x_norm, 0), (x_norm, height), (255, 255, 0), 2) # x_norm
-        cv2.line(frame, (width//2, 0), (width//2, height), (0, 255, 255), 2) # ROI
+        cv2.line(frame, (roi_border, 0), (roi_border, height), (0, 255, 255), 2) # ROI
         
         #cv2.imshow("Grayscale", grayscale)
         cv2.imshow("Lines", frame)
 
-        if cv2.waitKey(33) & 0xFF == ord('q'):
+        if cv2.waitKey(33) & 0xFF == ord('q'): # ongeveer 30 FPS
             break
 
     capture.release()
     cv2.destroyAllWindows()
 
-path = 'C:/Vakken/Jaar 2/Project 78 (SDC)/data/smalldemofront/output_video.mp4'
+path = 'C:/Vakken/Jaar 2/Project 78 (SDC)/data/tests/testvideo2.mp4'
 detect_lanes(path)
